f72e9c71bdc193a8314d73e1b5c5dd5bbe230878
LasalleSprewell <133616710+LasalleSprewell@users.noreply.github.com>
2023-11-22 03:37:10 -0500

    Add comments to javascript

--- script.js
+++ script.js
@@ -3,6 +3,7 @@ const typeBox = document.getElementById("type-box");
 const list = document.getElementById("list");
 const clearout = document.getElementById("clearout");
 
+// Creates the individual list item
 function createListItem() {
     let li = document.createElement("li");
     li.appendChild(document.createTextNode(typeBox.value));
@@ -10,12 +11,13 @@ function createListItem() {
     typeBox.value = "";
 }
 
+//As long as the string isnt empty, it will add the list item by pressing the Enter button on your keyboard
 function addListAfterKeypress(event) {
     if (typeBox.value.length > 0 && event.keyCode === 13) {
         createListItem();
     }
 }
-
+//As long as the string isnt empty, it will add the list item by pressing the Submit button
 function addListAfterEnter() {
     if (typeBox.value.length > 0) {
         createListItem();
@@ -25,6 +27,7 @@ function addListAfterEnter() {
 submit.addEventListener("click", addListAfterEnter);
 typeBox.addEventListener("keypress", addListAfterKeypress);
 
+//Lets you delete all the list items by pressing "Clear Items"
 clearout.addEventListener("click", function(){
         while (list.firstChild) {
             list.removeChild(list.firstChild);
